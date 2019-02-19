58b2bb352a14c0aad785555f19d9f4e226fc55a5
Andrei Ihnatsiuk <33061150+garnich@users.noreply.github.com>
2019-02-19 13:45:42 +0300

    Add files via upload

---
+++ precache-manifest.919085ae08db472e7778b730dcf87677.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa4cf97a89d4c21ecfec",
+    "url": "/dashboard/static/js/runtime~main.fa4cf97a.js"
+  },
+  {
+    "revision": "965a8a9abe3f4ced5bd6",
+    "url": "/dashboard/static/js/main.965a8a9a.chunk.js"
+  },
+  {
+    "revision": "cb17bac7ef7dac13e7c4",
+    "url": "/dashboard/static/js/2.cb17bac7.chunk.js"
+  },
+  {
+    "revision": "965a8a9abe3f4ced5bd6",
+    "url": "/dashboard/static/css/main.d6521a26.chunk.css"
+  },
+  {
+    "revision": "0fc4b93096b8d2eca3edfcbe319ff8c9",
+    "url": "/dashboard/index.html"
+  }
+];
